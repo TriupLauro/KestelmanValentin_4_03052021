6dc2c95d9b17fb23d507e5fdc1193b5b51b9952e
Valentin Kestelman <valaourok@gmail.com>
2021-05-03 14:32:42 +0200

    Fork, copie des fichiers et bouton "x"

---
+++ dev/modal.js
@@ -0,0 +1,32 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+// "X" button (close modal)
+const closeBtn = document.querySelector("span.close");
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
