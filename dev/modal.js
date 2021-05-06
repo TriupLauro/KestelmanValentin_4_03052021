20792fbf0743d3bed5553e355c3970d7b0236c2e
Valentin Kestelman <valaourok@gmail.com>
2021-05-06 18:04:35 +0200

    Validation, un seul message d'erreur

--- dev/modal.js
+++ dev/modal.js
@@ -18,8 +18,21 @@ function oneDigitAtLeast(value) {
   return /\d+/.test(value);
 }
 
+// Go trhought a least of radio inputs and returns true if one is checked
+function oneRadioCheckedAtLeast(array) {
+  //console.log("on vérifie les inputs radio");
+  for (var i = 0; i < array.length; i++) {
+    //console.log(array[i]);
+    if (array[i].checked  == true) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // Regex for validating e-mail (regex from https://www.w3resource.com/javascript/form/email-validation.php) maybe wrong
 // May actually reject some valid addresses
+// Not actually called in the rest of the code (using validation API instead)
 function validEmail(value) {
   return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value);
 }
@@ -33,11 +46,17 @@ const closeBtn = document.querySelector("span.close");
 // First and last name input
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
-// // E-mail input
+// E-mail input
 const email = document.getElementById("email");
-// // Number of past contests input
-// const quantity = document.getElementById("quantity");
-
+// Birthday input
+const birth = document.getElementById("birthdate");
+console.log(birth);
+// Number of past contests input
+const quantity = document.getElementById("quantity");
+// List of radio inputs, city
+const cityRadio = reserve.location;
+// The TOS checkbox
+const tosCheck = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -55,9 +74,7 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// Events triggered to set custom validity message
-// When changing the curstom message, prevent the form from submitting
-// So not using them
+// Events triggered to set error message
 // firstName.addEventListener("input", validateName);
 // lastName.addEventListener("input", validateName);
 // email.addEventListener("input", validateEmail);
@@ -98,29 +115,73 @@ function validate() {
   }
 
   // Validating email
+  // Abort if empty
   if (email.value == "") {
     console.log("email non renseigné");
     formData[2].setAttribute("data-error", "Veuillez renseigner votre adresse e-mail");
     formData[2].setAttribute("data-error-visible", "true");
     return false;
   }
-  if (validEmail(email.value)) {
-    console.log("email valide");
-    formData[1].removeAttribute("data-error");
-    formData[1].setAttribute("data-error-visible", "false");
-  } else {
+  console.log(email.validity);
+  // Uses the validation API for correct e-mail format
+  // elt.validity.typeMismatch is true if format is incorrect
+  if (email.validity.typeMismatch) {
     console.log("email non valide");
     formData[2].setAttribute("data-error", "Adresse e-mail invalide");
     formData[2].setAttribute("data-error-visible", "true");
     return false;
+  } else {
+    console.log("email valide");
+    formData[2].removeAttribute("data-error");
+    formData[2].setAttribute("data-error-visible", "false");
   }
+
+  // Check if date as been entered
+  console.log(birth);
+  if (birth.value == "") {
+    formData[3].setAttribute("data-error", "Veuillez renseigner votre date de naissance");
+    formData[3].setAttribute("data-error-visible", "true");
+    return false;
+  } else {
+    formData[3].removeAttribute("data-error");
+    formData[3].setAttribute("data-error-visible", "false");
+  }
+  // Validate past contest number
+  let contestNumber = quantity.value;
+  if (oneDigitAtLeast(contestNumber) && (contestNumber != "")) {
+    console.log("nombre valide");
+    formData[4].removeAttribute("data-error");
+    formData[4].setAttribute("data-error-visible", "false");
+  } else { 
+    console.log("nombre invalide");
+    formData[4].setAttribute("data-error", "Veuillez entrer un nombre");
+    formData[4].setAttribute("data-error-visible", "true");
+    return false;
+  }
+
+  // Check if one location option is selected
+  if (!oneRadioCheckedAtLeast(cityRadio)) {
+    formData[5].setAttribute("data-error", "Veuillez choisir une ville");
+    formData[5].setAttribute("data-error-visible", "true");
+    return false;
+  } else {
+    formData[5].removeAttribute("data-error");
+    formData[5].setAttribute("data-error-visible", "false");
+  }
+
+  // Check if TOS have been approved
+  if (tosCheck.checked != true) {
+    formData[6].setAttribute("data-error", "Il faut approuver les termes et conditions d'utilisations");
+    formData[6].setAttribute("data-error-visible", "true");
+    return false;
+  } else {
+    formData[6].removeAttribute("data-error");
+    formData[6].setAttribute("data-error-visible", "false");
+  }
+
+  //All checks passed !
 }
 
-// const firstErrorElt = document.createElement("p");
-// formData[0].appendChild(firstErrorElt);
-// firstErrorElt.innerText = twoCharsErrorMsg;
-// firstErrorElt.classList.add("text-control");
-// formData[0].setAttribute("data-error-visible","true");
 
 // JavaScript validation with curstom error message
 // Initialize all inputs with error message
